7d1c90e169d44329e7bbdc3366b0b2d21436c249
Sven Anders Robbestad <sven.anders.robbestad@acando.no>
2017-11-07 11:47:57 +0100

    Add prettier

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ import PropTypes from "prop-types"
 import objectAssign from "object-assign"
 
 const Iframe = class extends PureComponent {
-  render() {
-    let props = {
-      ref: "iframe",
-      frameBorder: "0",
-      src: this.props.url,
-      target: "_parent",
-      allowFullScreen: this.props.allowFullScreen || false,
-      style: Object.assign({}, {
-        position: this.props.position || "absolute",
-        display: this.props.display || "block",
-        height: this.props.height || "100%",
-        width: this.props.width || "100%"
-      }, this.props.styles || {}),
-      height: this.props.height || "100%",
-      name: this.props.name || "",
-      width: this.props.width || "100%"
-    };
-    return React.createElement(
-      "iframe",
-      objectAssign(props, this.props.id ? ({id: this.props.id}) : {}, this.props.className ? ({className: this.props.className}) : {})
-    )
-  }
+	render() {
+		const props = {
+			ref: "iframe",
+			frameBorder: "0",
+			src: this.props.url,
+			target: "_parent",
+			allowFullScreen: this.props.allowFullScreen || false,
+			style: Object.assign({}, {
+				position: this.props.position || "absolute",
+				display: this.props.display || "block",
+				height: this.props.height || "100%",
+				width: this.props.width || "100%"
+			}, this.props.styles || {}),
+			height: this.props.height || "100%",
+			name: this.props.name || "",
+			width: this.props.width || "100%"
+		};
+		return React.createElement(
+			"iframe",
+			objectAssign(props, this.props.id ? ({id: this.props.id}) : {}, this.props.className ? ({className: this.props.className}) : {})
+		)
+	}
 }
 Iframe.propTypes = {
-  url: PropTypes.string.isRequired,
-  id: PropTypes.string,
-  className: PropTypes.string,
-  width: PropTypes.string,
-  position: PropTypes.string,
-  display: PropTypes.string,
-  name: PropTypes.string,
-  height: PropTypes.string,
-  styles: PropTypes.object,
-  allowFullScreen: PropTypes.bool
+	url: PropTypes.string.isRequired,
+	id: PropTypes.string,
+	className: PropTypes.string,
+	width: PropTypes.string,
+	position: PropTypes.string,
+	display: PropTypes.string,
+	name: PropTypes.string,
+	height: PropTypes.string,
+	styles: PropTypes.object,
+	allowFullScreen: PropTypes.bool
 }
 export default Iframe
