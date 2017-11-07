b20cde55019a05cdb0f4b7b26142770f6d91bdd3
Sven Anders Robbestad <sven.anders.robbestad@acando.no>
2017-11-07 14:23:40 +0100

    Add demo

---
+++ demo/index.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from "react"
+import ReactDOM from "react-dom"
+import Iframe from "./react-iframe.min.js"
+
+class Demo extends PureComponent {
+	render() {
+		return (
+			<Iframe
+				url="http://www.youtube.com/embed/xDMP3i36naA"
+				width="450px"
+				height="450px"
+				id="myId"
+				className="myClassname"
+				display="initial"
+				position="relative"
+				allowFullScreen
+			/>
+		)
+	}
+}
+
+ReactDOM.render(<Demo />, document.body)
