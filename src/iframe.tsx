61b225d2ab5fece95ff21e1438b09e9ce24e510f
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2022-09-26 11:13:34 +0200

    added new husky

--- src/iframe.tsx
+++ src/iframe.tsx
@@ -3,17 +3,37 @@ import objectAssign from "object-assign"
 
 import { IIframe } from "./types"
 
-const Iframe: ComponentType<IIframe>
-	= ({
-			 url, allowFullScreen, position, display,
-			 height, width, overflow, styles, onLoad,
-			 onMouseOver, onMouseOut, scrolling, id,
-			 frameBorder, ariaHidden, sandbox, allow,
-			 className, title, ariaLabel, ariaLabelledby,
-			 name, target, loading, importance, referrerpolicy,
-       allowpaymentrequest, src, key
-		 }: IIframe) => {
-
+const Iframe: ComponentType<IIframe> = ({
+	url,
+	allowFullScreen,
+	position,
+	display,
+	height,
+	width,
+	overflow,
+	styles,
+	onLoad,
+	onMouseOver,
+	onMouseOut,
+	scrolling,
+	id,
+	frameBorder,
+	ariaHidden,
+	sandbox,
+	allow,
+	className,
+	title,
+	ariaLabel,
+	ariaLabelledby,
+	name,
+	target,
+	loading,
+	importance,
+	referrerpolicy,
+	allowpaymentrequest,
+	src,
+	key
+}: IIframe) => {
 	const defaultProps = objectAssign({
 		src: src || url,
 		target: target || null,
@@ -41,8 +61,8 @@ const Iframe: ComponentType<IIframe>
 		height: height || null,
 		onLoad: onLoad || null,
 		onMouseOver: onMouseOver || null,
-    onMouseOut: onMouseOut || null,
-    key: key || "iframe"
+		onMouseOut: onMouseOut || null,
+		key: key || "iframe"
 	})
 	let props = Object.create(null)
 	for (let prop of Object.keys(defaultProps)) {
@@ -60,30 +80,29 @@ const Iframe: ComponentType<IIframe>
 	if (props.styles) {
 		for (let key of Object.keys(props.styles)) {
 			if (props.styles.hasOwnProperty(key)) {
-				props.style[key] = props.styles[key];
+				props.style[key] = props.styles[key]
 			}
 			if (Object.keys(props.styles).pop() == key) {
-				delete props.styles;
+				delete props.styles
 			}
 		}
 	}
 
-	if(allowFullScreen){
-		if("allow" in props){
-			const currentAllow = props.allow.replace("fullscreen","")
-			props.allow = `fullscreen ${currentAllow.trim()}`.trim()	
+	if (allowFullScreen) {
+		if ("allow" in props) {
+			const currentAllow = props.allow.replace("fullscreen", "")
+			props.allow = `fullscreen ${currentAllow.trim()}`.trim()
 		} else {
 			props.allow = "fullscreen"
 		}
 	}
 
-	if(frameBorder >= 0){
-		if(!props.style.hasOwnProperty("border")){
+	if (frameBorder >= 0) {
+		if (!props.style.hasOwnProperty("border")) {
 			props.style.border = frameBorder
 		}
 	}
-	return <iframe {...props}/>
-
+	return <iframe {...props} />
 }
 
 export default Iframe
